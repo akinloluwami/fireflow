ad66e0271162d9cb83d36d89bc15779ad79afd91
Akinkunmi <akinkunmioye42@gmail.com>
2026-02-05 22:23:24 +0100

    feat(executor): add node executor exports index
    - Export executeIfElse condition node executor
    - Export executeSlack action node executor
    - Export executeDiscord action node executor
    - Export executeHttp action node executor
    - Export executeEmail action node executor
    - Export executeSetVariable transform node executor
    - Centralize node executor imports for cleaner module access

---
+++ src/lib/executor/nodes/index.ts
@@ -0,0 +1,7 @@
+// Node executor exports
+export { executeIfElse } from "./condition-if";
+export { executeSlack } from "./action-slack";
+export { executeDiscord } from "./action-discord";
+export { executeHttp } from "./action-http";
+export { executeEmail } from "./action-email";
+export { executeSetVariable } from "./transform-variable";
