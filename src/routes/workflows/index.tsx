05b6b50ea4cbcbdb3cf3c5e0c8acc6375530be03
Akinkunmi <akinkunmioye42@gmail.com>
2026-02-06 23:23:08 +0100

    Refactor WorkflowsListPage to ensure UserMenu displays user information correctly

--- src/routes/workflows/index.tsx
+++ src/routes/workflows/index.tsx
@@ -97,10 +97,6 @@ function WorkflowsListPage() {
     );
   }
 
-  if (!session?.user) {
-    return null;
-  }
-
   return (
     <div className="min-h-screen bg-gray-50">
       <header className="bg-white border-b border-gray-200">
@@ -111,7 +107,7 @@ function WorkflowsListPage() {
             </span>
           </Link>
           <div className="flex items-center gap-4">
-            <UserMenu user={session.user} />
+            <UserMenu user={session?.user!} />
           </div>
         </div>
       </header>
