02c79e6f2ca23cbb5c95df4adaed0f950b4b803e
Akinkunmi <akinkunmioye42@gmail.com>
2026-02-06 03:29:48 +0100

    chore: export SwitchConfig and LoopConfig from index

--- src/components/workflow/config/index.ts
+++ src/components/workflow/config/index.ts
@@ -3,5 +3,7 @@ export { SlackConfig } from "./SlackConfig";
 export { DiscordConfig } from "./DiscordConfig";
 export { WaitConfig } from "./WaitConfig";
 export { ConditionConfig } from "./ConditionConfig";
+export { SwitchConfig } from "./SwitchConfig";
+export { LoopConfig } from "./LoopConfig";
 export { VariablePicker } from "./VariablePicker";
 export { ExpressionInput, ExpressionTextarea } from "./ExpressionInput";
