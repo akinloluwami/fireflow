05139030d4969bbc8fb7fa2563c960edac1cd81d
Akinkunmi <akinkunmioye42@gmail.com>
2026-02-06 02:33:58 +0100

    chore: export new config components from index

--- src/components/workflow/config/index.ts
+++ src/components/workflow/config/index.ts
@@ -2,3 +2,6 @@ export { TriggerConfig } from "./TriggerConfig";
 export { SlackConfig } from "./SlackConfig";
 export { DiscordConfig } from "./DiscordConfig";
 export { WaitConfig } from "./WaitConfig";
+export { ConditionConfig } from "./ConditionConfig";
+export { VariablePicker } from "./VariablePicker";
+export { ExpressionInput, ExpressionTextarea } from "./ExpressionInput";
